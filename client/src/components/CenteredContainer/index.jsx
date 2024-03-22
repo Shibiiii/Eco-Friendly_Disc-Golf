3f27a1dd2ddd74e5870b569edc0eab87edc50f20
Jarede712 <jaredeic@yahoo.com>
2024-03-21 21:12:07 -0700

    added centered container component and added concurrently to dev dependiencies to allow develop command to run

---
+++ client/src/components/CenteredContainer/index.jsx
@@ -0,0 +1,16 @@
+function CenteredContainer({ children }) {
+  return (
+    <div
+      style={{
+        height: 560,
+        clear: 'both',
+        paddingTop: 120,
+        textAlign: 'center',
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default CenteredContainer;
