f025e7d189b7164753172f63103426e8ed74ed62
Shibiiii <purplejohn13@gmail.com>
2024-03-25 19:15:19 -0700

    use efffect written for updating product

--- client/src/components/ProductList/index.jsx
+++ client/src/components/ProductList/index.jsx
@@ -5,4 +5,24 @@ import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
-import spinner from '../../assets/spinner.gif';
+
+function ProductList() {
+	const [state, dispatch] = useStoreContext();
+
+	const { currentCategory } = state;
+
+	const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+	useEffect(() => {
+		if (data) {
+			dispatch({
+				type: UPDATE_PRODUCTS,
+				products: data.products,
+			});
+			data.products.forEach((product) => {
+				idbPromise('products', 'put', product);
+			});
+		}
+		e;
+	});
+}
